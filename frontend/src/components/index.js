61dfa5368e997e08c828a98e694ae95651b62238
Nakul Shaw <nakulkelvin@gmail.com>
2026-01-06 22:10:47 +0530

    auth

---
+++ frontend/src/components/index.js
@@ -0,0 +1,15 @@
+import AuthLayout from "./AuthLayout.jsx"
+import Button from "./Button.jsx"
+import Input from "./Input.jsx"
+import Login from "./Login.jsx"
+import Logo from "./Logo.jsx"
+import Signup from "./Signup.jsx"
+
+export {
+    AuthLayout,
+    Button,
+    Input,
+    Login,
+    Logo,
+    Signup
+}
